4a1200b5d3865b0f1b8a7d28cbc20f4b32bce803
anshulprakash <anshul.prakash17@gmail.com>
2017-06-18 20:08:36 -0500

    adding example code

---
+++ projects/boston_housing/example.py
@@ -0,0 +1,16 @@
+from sklearn import datasets # sklearn comes with a variety of preloaded datasets 
+from sklearn import metrics # calculate how well our model is doing
+from sklearn.linear_model import LinearRegression
+
+
+housing_data = datasets.load_boston()
+
+linear_regression_model = LinearRegression()
+
+linear_regression_model.fit(housing_data.data, housing_data.target)
+
+predictions = linear_regression_model.predict(housing_data.data)
+
+score = metrics.r2_score(housing_data.target, predictions)
+
+print(score)
