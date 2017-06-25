41ef2a5fb31762821b27a354e7059735d4f11ced
anshulprakash <anshul.prakash17@gmail.com>
2017-06-24 21:58:48 -0500

    added commands to exlore data

--- projects/ud120-projects-master/ud120-projects-master/datasets_questions/explore_enron_data.py
+++ projects/ud120-projects-master/ud120-projects-master/datasets_questions/explore_enron_data.py
@@ -19,4 +19,33 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+count = 0
+count_salary = 0
+count_email = 0
+count_payments = 0
+count_poi_payments = 0
+number_of_entries = len(enron_data)
+
+for key, value in enron_data.items():
+    #print(key, len([item for item in value if item]))
+    if(enron_data[key]['poi']):
+    	count = count + 1
+    if(enron_data[key]['poi'] & (enron_data[key]['salary'] != 'NaN')):
+    	count_poi_payments = count_poi_payments + 1
+    if(enron_data[key]['salary']!= 'NaN'):
+    	count_salary = count_salary + 1
+    if(enron_data[key]['email_address']!= 'NaN'):
+    	count_email = count_email + 1
+    if(enron_data[key]['total_payments']!= 'NaN'):
+    	count_payments = count_payments + 1
+
+
+print("Number of POIs = "+ str(count))
+#print(enron_data["PRENTICE JAMES"].keys())
+print("Number of salaries = "+ str(count_salary))
+print("Number of email addresses = "+ str(count_email))
+print(count_payments)
+print(number_of_entries)
+print(count_poi_payments)
+
 
